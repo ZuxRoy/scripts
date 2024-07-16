dc64aaffe469b008cf18166737d5f8103d6f4933
ZuxRoy <snehashis.roysr@gmail.com>
2024-07-16 13:36:26 +0530

    memory script update

--- memory-noti.sh
+++ memory-noti.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-mem=$(free -m | awk '/^Mem:/ {printf "Used Memory \t: %.2f GB\nTotal Memory \t: %.2f GB\n", $2/1024, $3/1024}')
+mem=$(free -m | awk '/^Mem:/ {printf "Total Memory \t: %.2f GB\nUsed Memory \t: %.2f GB\nFree memory \t: %.2f GB\n", $2/1024, $3/1024, $4/1024}')
 dunstify "$mem" -u normal -t 5000 -r 9991 -i "$HOME/.local/share/icons/custom/memory.svg"
